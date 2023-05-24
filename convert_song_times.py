d687e51b1e90c4eb1f8855d0a60c3d8f53a3f17f
captainjetski <49173784+m-glisson@users.noreply.github.com>
2023-05-24 17:56:53 -0400

    Update convert_song_times.py to make a sub directory and run through all the files

--- convert_song_times.py
+++ convert_song_times.py
@@ -1,21 +1,41 @@
 import re 
 import datetime
+import os
 
 
-sourceFile = open("OR Song Times.txt", "r")
+directory = os.path.dirname(os.path.abspath(__file__))
 
-print("okay")
-
-parsedFile  = open('parsed_song_times.txt', 'w'); 
-for line in sourceFile: 
-    parsedData = line 
-    # matchString = '(\d+-\d+)'
-    if re.match( ".+(\d+-\d+).+", parsedData):
-        seconds = re.search( '(\d+-\d+)', parsedData).group(0)   
-        start, stop = seconds.split('-'); 
-        tstart= str(datetime.timedelta(seconds=int(start)))
-        tstop= str(datetime.timedelta(seconds=int(stop)))
-        parsedData = parsedData.replace(seconds, f"{tstart}-{tstop}")
+# Construct the path for the new directory
+new_directory_path = os.path.join(directory, 'results')
+# Create the new directory 
+if os.path.isdir(new_directory_path) != True:
+    os.mkdir(new_directory_path)
 
 
-    parsedFile.write(parsedData)
+# Get a list of all files in the directory
+file_list = os.listdir(directory)
+
+# Iterate over each file
+for file_name in file_list:
+    # Create the absolute path of the file
+    file_path = os.path.join(directory, file_name)
+    
+    # Check if the path is a file (not a directory)
+    if os.path.isfile(file_path) and file_name.find('.txt') != -1 :
+        sourceFile = open(file_name, "r")
+        parsedFile  = open(f"results/converted{file_name}", 'w'); 
+        for line in sourceFile: 
+            parsedData = line 
+            # matchString = '(\d+-\d+)'
+            if re.match( ".+(\d+-\d+).+", parsedData):
+                seconds = re.search( '(\d+-\d+)', parsedData).group(0)   
+                start, stop = seconds.split('-'); 
+                tstart= str(datetime.timedelta(seconds=int(start)))
+                tstop= str(datetime.timedelta(seconds=int(stop)))
+                parsedData = parsedData.replace(seconds, f"{tstart}-{tstop}")
+
+
+            parsedFile.write(parsedData)
+        
+
+
