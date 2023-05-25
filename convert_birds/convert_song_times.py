fbc2539a7c9bfd2675254fce6759bf497bf0a8a4
m-glisson <49173784+m-glisson@users.noreply.github.com>
2023-05-24 23:01:51 -0400

    update to handle unaccounted for decimals

--- convert_birds/convert_song_times.py
+++ convert_birds/convert_song_times.py
@@ -22,17 +22,21 @@ for file_name in file_list:
     
     # Check if the path is a file (not a directory)
     if os.path.isfile(file_path) and file_name.find('.txt') != -1 :
-        sourceFile = open(file_name, "r")
-        parsedFile  = open(f"results/converted{file_name}", 'w'); 
+        sourceFile = open(file_path, "r")
+        parsedFile  = open(f"results/converted_{file_name}", 'w'); 
         for line in sourceFile: 
             parsedData = line 
             # matchString = '(\d+-\d+)'
-            if re.match( ".+(\d+-\d+).+", parsedData):
-                seconds = re.search( '(\d+-\d+)', parsedData).group(0)   
+            if re.match( ".+(\d+(.\d)?-\d+(.\d)?).+", parsedData):
+                seconds = re.search( '(\d+(.\d)?-\d+(.\d)?)', parsedData).group(0)   
                 start, stop = seconds.split('-'); 
-                tstart= str(datetime.timedelta(seconds=int(start)))
-                tstop= str(datetime.timedelta(seconds=int(stop)))
+                tstart= str(datetime.timedelta(seconds=float(start)))
+                tstop= str(datetime.timedelta(seconds=float(stop)))
                 parsedData = parsedData.replace(seconds, f"{tstart}-{tstop}")
+            elif bool(re.search( "\s(\d+(.\d)?)(\s|\n)", parsedData)):
+                seconds = re.search( "\s(\d+(.\d)?)(\s|\n)", parsedData).group(1)   
+                tstart= str(datetime.timedelta(seconds=float(seconds)))
+                parsedData = parsedData.replace(seconds, f"{tstart}")
 
 
             parsedFile.write(parsedData)
