1c081896a1270c81a7fe2ed468b1a35e2a7b3ecf
matt g <matthew.g@teachingstrategies.com>
2024-02-15 19:13:15 -0500

    labels with mock data

--- visualize/public/static/js/index.js
+++ visualize/public/static/js/index.js
@@ -1,113 +1,137 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
-import axios from 'https://cdn.skypack.dev/axios';
+import axios from "https://cdn.skypack.dev/axios";
 
 const dataSet = async function getData() {
-    return await axios.get('/api/markov-chain');
-}
+  return await axios.get("/api/markov-chain");
+};
 async function drawChart() {
-    const resp = await dataSet();
+  const resp = await dataSet();
 
-    const data = resp.data
+  const data = resp.data;
 
-    const svgWidth = 500;
-    const svgHeight = 500;
-    const barPadding = 5;
-    const barWidth = svgWidth / data.length;
+  const svgWidth = 500;
+  const svgHeight = 500;
+  const barPadding = 5;
+  const barWidth = svgWidth / data.length;
 
-    const width = 928;
-    const height = 600;
-  
-    // Specify the color scale.
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-  
-    // The force simulation mutates links and nodes, so create a copy
-    // so that re-evaluating this cell produces the same result.
-    const links = data.links.map(d => ({...d}));
-    const nodes = data.nodes.map(d => ({...d}));
-  
-    // Create a simulation with several forces.
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2))
-        .on("tick", ticked);
-  
-    // Create the SVG container.
+  const width = 928;
+  const height = 600;
 
-    let svg = d3.select("svg");
-    
-    svg
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
-  
-    // Add a line for each link, and a circle for each node.
-    const link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-      .selectAll()
-      .data(links)
-      .join("line")
-        .attr("stroke-width", d => Math.sqrt(d.value));
-  
-    const node = svg.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-      .selectAll()
-      .data(nodes)
-      .join("circle")
-        .attr("r", 5)
-        .attr("fill", d => color(d.group));
-  
-    node.append("title")
-        .text(d => d.id);
-  
-    // Add a drag behavior.
-    node.call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
-  
-    // Set the position attributes of links and nodes each time the simulation ticks.
-    function ticked() {
-      link
-          .attr("x1", d => d.source.x)
-          .attr("y1", d => d.source.y)
-          .attr("x2", d => d.target.x)
-          .attr("y2", d => d.target.y);
-  
-      node
-          .attr("cx", d => d.x)
-          .attr("cy", d => d.y);
-    }
-  
-    // Reheat the simulation when drag starts, and fix the subject position.
-    function dragstarted(event) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
-    }
-  
-    // Update the subject (dragged node) position during drag.
-    function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
-    }
-  
-    // Restore the target alpha so the simulation cools after dragging ends.
-    // Unfix the subject position now that it’s no longer being dragged.
-    function dragended(event) {
-      if (!event.active) simulation.alphaTarget(0);
-      event.subject.fx = null;
-      event.subject.fy = null;
-    }
-  
-    // When this cell is re-run, stop the previous simulation. (This doesn’t
-    // really matter since the target alpha is zero and the simulation will
-    // stop naturally, but it’s a good practice.)
-    // invalidation.then(() => simulation.stop());
-  
-    // return svg.node();
+  // Specify the color scale.
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+  // The force simulation mutates links and nodes, so create a copy
+  // so that re-evaluating this cell produces the same result.
+  const links = data.links.map((d) => ({ ...d }));
+  const nodes = data.nodes.map((d) => ({ ...d }));
+
+  // Create a simulation with several forces.
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force(
+      "link",
+      d3.forceLink(links).id((d) => d.id)
+    )
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", ticked);
+
+  // Create the SVG container.
+
+  let svg = d3.select("svg");
+
+  svg
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .attr("style", "max-width: 100%; height: auto;");
+
+  svg
+    .append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("viewBox", "-0 -5 10 10")
+    .attr("refX", 5)
+    .attr("refY", 0)
+    .attr("orient", "auto")
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("xoverflow", "visible")
+    .append("svg:path")
+    .attr("d", "M 0,-5 L 10 ,0 L 0,5")
+    .attr("fill", "#999")
+    .style("stroke", "none");
+
+  // Add a line for each link, and a circle for each node.
+  const link = svg
+    .append("g")
+    .attr("id", "arrow")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll()
+    .data(links)
+    .join("line")
+    .attr("stroke-width", (d) => Math.sqrt(d.value))
+    .attr("marker-end", "url(#arrowhead)"); // reference the marker here
+
+  const node = svg
+    .append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+    .selectAll()
+    .data(nodes)
+    .join("circle")
+    .attr("r", 5);
+  // .attr("fill", d => color(d.group));
+
+  node.append("title").text((d) => d.id);
+
+  // Create text labels
+  const label = svg
+    .append("g")
+    .selectAll("text")
+    .data(nodes)
+    .join("text")
+    .text((d) => d.id) // replace d.id with the property you want to display
+    .attr("dx", ".10em") // dx and dy are offsets from the circle's center
+    .attr("dy", ".10em");
+
+  // Add a drag behavior.
+  node.call(
+    d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
+  );
+
+  // Set the position attributes of links and nodes each time the simulation ticks.
+  function ticked() {
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
+
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    label.attr("x", (d) => d.x).attr("y", (d) => d.y);
+  }
+
+  // Reheat the simulation when drag starts, and fix the subject position.
+  function dragstarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
+
+  // Update the subject (dragged node) position during drag.
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
+
+  // Restore the target alpha so the simulation cools after dragging ends.
+  // Unfix the subject position now that it’s no longer being dragged.
+  function dragended(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+  }
 }
 drawChart();
